55d6c150e047a0f86698420672587f026fd72e85
AlexandreFrolov <alexandre@frolov.pp.ru>
2023-07-19 13:54:23 +0300

    Update led_async.py

--- led_async.py
+++ led_async.py
@@ -5,52 +5,36 @@ import traceback
 from ricontroller import RiController
 from riled import RiLed
 
+def print_led_state(led):
+    color = led.get_color()
+    print(f"Led color: : {str(color['red'].value)}, {str(color['green'].value)}, {str(color['blue'].value)}")
+    print(f"Led state: : {str(led.get_state().value)}")
+
 if __name__ == "__main__":
     try:
         controller = RiController(c_bool(True))
-        print(f"Model: {controller.model_name}")
+        print(f"Controller Model: {controller.model_name}")
         controller.init()
 
         led = RiLed(controller)
-        print(f"Model: {led.model_name}")
-
-
+        print(f"LED Model: {led.model_name}")
 
         led.add(14, 15, 13)
-
-        print("Led state: " + str(led.get_state()))
+        print_led_state(led)
 
         led.pulse(255, 0, 0, 1500)
+        time.sleep(0.3) 
+        print_led_state(led)
         time.sleep(1) 
+
         led.pulse(0, 255, 0, 1500)
+        time.sleep(0.3) 
+        print_led_state(led)
         time.sleep(1) 
+
         led.pulse(0, 0, 255, 1500)
-        time.sleep(1) 
-
-        print("Led state: " + str(led.get_state()))
-
-        color = led.get_color()
-
-        print("Led color: ")
-        print(color)
-
-        led.flicker(255, 0, 0, 500, 5)
-        led.flicker(0, 255, 0, 500, 5)
-        led.flicker(0, 0, 255, 500, 5)
-
-        led.pulse(255, 0, 0, 500)
-        led.pulse(0, 255, 0, 500)
-        led.pulse(0, 0, 255, 500)
-
-
-        led.pulse_pause(255, 0, 0, 1000, 200, 3)
-        led.pulse_pause(0, 255, 0, 1000, 200, 3)
-        led.pulse_pause(0, 0, 255, 1000, 200, 3)
-
-        led.pulse_frequency(255, 0, 0, 10, 10)
-        led.pulse_frequency(0, 255, 0, 20, 10)
-        led.pulse_frequency(0, 0, 255, 30, 10)
-
+        time.sleep(0.3) 
+        print_led_state(led)
         time.sleep(1) 
 
         led.stop()
