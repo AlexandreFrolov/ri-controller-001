9745364bb36af9cd9efa15ddf94736ec3defad89
AlexandreFrolov <alexandre@frolov.pp.ru>
2023-07-19 09:29:09 +0300

    Fix

---
+++ ricontroller.py
@@ -0,0 +1,73 @@
+import sys
+import time
+import platform
+from ctypes import *
+import traceback
+
+class RiController:
+    def __init__(self, is_async: c_bool):
+        self.model_name = "RoboIntellect controller 001"
+        self.platform = platform.system()
+        try:
+            if self.platform == "Windows":
+                self.lib = cdll.LoadLibrary("C:\Windows\system32\librisdk.dll")
+            if self.platform == "Linux":
+                self.lib = cdll.LoadLibrary("/usr/local/robohand_remote_control/librisdk.so")
+        except OSError as e:
+            raise Exception("Failed to load: " + str(e))
+        self.errTextC = create_string_buffer(1000)
+        self.i2c = c_int()
+        self.pwm = c_int()
+        self.is_async = is_async
+
+    def err_msg(self):
+        return(self.errTextC.raw.decode())
+
+    def async_on(self):
+        self.is_async = c_bool(True)         
+
+    def async_off(self):
+        self.is_async = c_bool(False)         
+
+    def init(self):
+        self.lib.RI_SDK_InitSDK.argtypes = [c_int, c_char_p]
+        self.lib.RI_SDK_CreateBasic.argtypes = [POINTER(c_int), c_char_p]
+        self.lib.RI_SDK_CreateModelComponent.argtypes = [c_char_p, c_char_p, c_char_p, POINTER(c_int), c_char_p]
+        self.lib.RI_SDK_LinkPWMToController.argtypes = [c_int, c_int, c_uint8, c_char_p]
+        descriptor = c_int()
+
+        errCode = self.lib.RI_SDK_InitSDK(1, self.errTextC)
+        if errCode != 0:
+            raise Exception(f"init: RI_SDK_InitSDK failed with error code {errCode}: {self.err_msg()}")
+
+        errCode = self.lib.RI_SDK_CreateBasic(descriptor, self.errTextC)
+        if errCode != 0:
+            raise Exception(f"init: RI_SDK_CreateBasic failed with error code {errCode}: {self.err_msg()}")
+
+        errCode = self.lib.RI_SDK_CreateModelComponent("connector".encode(), "pwm".encode(), "pca9685".encode(), self.pwm, self.errTextC)
+        if errCode != 0:
+            raise Exception(f"init: RI_SDK_CreateModelComponent failed with error code {errCode}: {self.err_msg()}")
+
+        errCode = self.lib.RI_SDK_CreateModelComponent("connector".encode(), "i2c_adapter".encode(), "ch341".encode(), self.i2c, self.errTextC)
+        if errCode != 0:
+            raise Exception(f"init: RI_SDK_CreateModelComponent failed with error code {errCode}: {self.err_msg()}")
+
+        errCode = self.lib.RI_SDK_LinkPWMToController(self.pwm, self.i2c, c_uint8(0x40), self.errTextC)
+        if errCode != 0:
+            raise Exception(f"init: RI_SDK_LinkPWMToController failed with error code {errCode}: {self.err_msg()}")
+
+    def cleanup(self):
+        self.lib.RI_SDK_sigmod_PWM_ResetAll.argtypes = [c_int, c_char_p]
+        self.lib.RI_SDK_DestroyComponent.argtypes = [c_int, c_char_p]
+        self.lib.RI_SDK_DestroySDK.argtypes = [c_bool, c_char_p]
+        errCode = self.lib.RI_SDK_sigmod_PWM_ResetAll(self.pwm, self.errTextC)
+        if errCode != 0:
+            raise Exception(f"cleanup_final: RI_SDK_sigmod_PWM_ResetAll failed with error code {errCode}: {self.err_msg()}")
+
+        errCode = self.lib.RI_SDK_DestroyComponent(self.i2c, self.errTextC)
+        if errCode != 0:
+            raise Exception(f"cleanup_final: RI_SDK_DestroyComponent failed with error code {errCode}: {self.err_msg()}")
+
+        errCode = self.lib.RI_SDK_DestroySDK(True, self.errTextC)
+        if errCode != 0:
+            raise Exception(f"cleanup_final: RI_SDK_DestroySDK failed with error code {errCode}: {self.err_msg()}")
---
+++ riled.py
@@ -0,0 +1,63 @@
+import sys
+import time
+import platform
+from ctypes import *
+from ricontroller import RiController
+import traceback
+
+class RiLed:
+    def __init__(self, controller):
+        self.controller = controller
+        self.model_name = "RoboIntellect LED"
+        self.errTextC = create_string_buffer(1000)
+        self.led = c_int()
+
+    def err_msg(self):
+        return(self.errTextC.raw.decode())
+
+    def add(self, r, g, b):
+        self.controller.lib.RI_SDK_CreateModelComponent.argtypes = [c_char_p, c_char_p, c_char_p, POINTER(c_int), c_char_p]
+        self.controller.lib.RI_SDK_LinkLedToController.argtypes = [c_int, c_int, c_int, c_int, c_int, c_char_p]
+        errCode = self.controller.lib.RI_SDK_CreateModelComponent("executor".encode(), "led".encode(), "ky016".encode(), self.led, self.errTextC)
+        if errCode != 0:
+            raise Exception(f"RI_SDK_CreateModelComponent failed with error code {errCode}: {self.err_msg()}")
+    #    errCode = self.lib.RI_SDK_LinkLedToController(led, self.controller.pwm, 15, 14, 13, self.errTextC)
+        errCode = self.controller.lib.RI_SDK_LinkLedToController(self.led, self.controller.pwm, r, g, b, self.errTextC)
+        if errCode != 0:
+            raise Exception(f"RI_SDK_LinkLedToController failed with error code {errCode}: {self.err_msg()}")
+
+    def stop(self):
+        self.controller.lib.RI_SDK_exec_RGB_LED_Stop.argtypes = [c_int, c_char_p]
+        errCode = self.controller.lib.RI_SDK_exec_RGB_LED_Stop(self.led, self.errTextC)
+        if errCode != 0:
+            raise Exception(f"RI_SDK_exec_RGB_LED_Stop failed with error code {errCode}: {self.err_msg()}")
+
+    def cleanup(self):
+        self.controller.lib.RI_SDK_DestroyComponent.argtypes = [c_int, c_char_p]
+        errCode = self.controller.lib.RI_SDK_DestroyComponent(self.led, self.errTextC)
+        if errCode != 0:
+            raise Exception(f"RI_SDK_DestroyComponent failed with error code {errCode}: {self.err_msg()}")
+
+    def pulse(self, r, g, b, duration):
+        self.controller.lib.RI_SDK_exec_RGB_LED_SinglePulse.argtypes = [c_int, c_int, c_int, c_int, c_int, c_bool, c_char_p]
+        errCode = self.controller.lib.RI_SDK_exec_RGB_LED_SinglePulse(self.led, r, g, b, duration, self.controller.is_async, self.errTextC)
+        if errCode != 0:
+            raise Exception(f"RI_SDK_exec_RGB_LED_SinglePulse failed with error code {errCode}: {self.err_msg()}")
+
+    def pulse_pause(self, r, g, b, duration, pause, limit):
+        self.controller.lib.RI_SDK_exec_RGB_LED_FlashingWithPause.argtypes = [c_int, c_int, c_int, c_int, c_int, c_int, c_int, c_bool, c_char_p]
+        errCode = self.controller.lib.RI_SDK_exec_RGB_LED_FlashingWithPause(self.led, r, g, b, duration, pause, limit, self.controller.is_async, self.errTextC)
+        if errCode != 0:
+            raise Exception(f"RI_SDK_exec_RGB_LED_FlashingWithPause failed with error code {errCode}: {self.err_msg()}")
+
+    def pulse_frequency(self, r, g, b, frequency, limit):
+        self.controller.lib.RI_SDK_exec_RGB_LED_FlashingWithFrequency.argtypes = [c_int, c_int, c_int, c_int, c_int, c_int, c_bool, c_char_p]
+        errCode = self.controller.lib.RI_SDK_exec_RGB_LED_FlashingWithFrequency(self.led, r, g, b, frequency, limit, self.controller.is_async, self.errTextC)
+        if errCode != 0:
+            raise Exception(f"RI_SDK_exec_RGB_LED_FlashingWithFrequency failed with error code {errCode}: {self.err_msg()}")
+
+    def flicker(self, r, g, b, duration, limit):
+        self.controller.lib.RI_SDK_exec_RGB_LED_Flicker.argtypes = [c_int, c_int, c_int, c_int, c_int, c_int, c_bool, c_char_p]
+        errCode = self.controller.lib.RI_SDK_exec_RGB_LED_Flicker(self.led, r, g, b, duration, limit, self.controller.is_async, self.errTextC)
+        if errCode != 0:
+            raise Exception(f"RI_SDK_exec_RGB_LED_Flicker failed with error code {errCode}: {self.err_msg()}")
