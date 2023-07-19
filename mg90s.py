20cd1b93816837664e944e78e5edaca873e69900
AlexandreFrolov <alexandre@frolov.pp.ru>
2023-07-19 15:07:37 +0300

    Update mg90s.py

--- mg90s.py
+++ mg90s.py
@@ -17,38 +17,38 @@ if __name__ == "__main__":
 
         print("\nMG90S поворот в крайние положения")
 
-        servo_1.rotate(0, 200) # 2320 mc на осциллографе
+        servo_1.rotate(0, 200)
         time.sleep(2) 
-        print("servo_1 angle: " + str(servo_1.get_angle())) # угол 229
+        print("servo_1 angle: " + str(servo_1.get_angle()))
 
-        servo_1.rotate(1, 200) # 330 mc на осциллографе
+        servo_1.rotate(1, 200)
         time.sleep(2) 
-        print("servo_1 angle: " + str(servo_1.get_angle())) # угол 0
+        print("servo_1 angle: " + str(servo_1.get_angle()))
 
-        servo_1.set_middle() # 1330 mc на осциллографе
+        servo_1.set_middle()
         time.sleep(2) 
-        print("servo_1 angle: " + str(servo_1.get_angle())) # угол 114
+        print("servo_1 angle: " + str(servo_1.get_angle()))
 
         print("\nMG90S управление через длительность импульсов")
 
-        servo_1.turn_by_pulse(2650) # 2550 mc на осциллографе
+        servo_1.turn_by_pulse(2650)
         time.sleep(2) 
-        print("servo_1 angle: " + str(servo_1.get_angle())) # угол 229 (custom 207)
+        print("servo_1 angle: " + str(servo_1.get_angle()))
 
-        servo_1.turn_by_pulse(365) # 330 mc на осциллографе
+        servo_1.turn_by_pulse(365)
         time.sleep(2) 
-        print("servo_1 angle: " + str(servo_1.get_angle())) # угол 0
+        print("servo_1 angle: " + str(servo_1.get_angle()))
         
-        servo_1.turn_by_pulse(1500) # 1430 mc на осциллографе
+        servo_1.turn_by_pulse(1500)
         time.sleep(2) 
-        print("servo_1 angle: " + str(servo_1.get_angle())) # угол 114 (custom 103)
+        print("servo_1 angle: " + str(servo_1.get_angle()))
         
 
         print("\nMG90S Минимальный шаг")
 
-        servo_1.set_middle() # 1330 mc на осциллографе
+        servo_1.set_middle()
         time.sleep(2) 
-        print("servo_1 angle: " + str(servo_1.get_angle())) # угол 114
+        print("servo_1 angle: " + str(servo_1.get_angle()))
 
 
         servo_1.rotate_min_step(1, 100)
@@ -75,9 +75,9 @@ if __name__ == "__main__":
      
         print("\nMG90S поворот на заданный угол")
 
-        servo_1.set_middle() # 1330 mc на осциллографе
+        servo_1.set_middle()
         time.sleep(2) 
-        print("servo_1 angle: " + str(servo_1.get_angle())) # угол 114
+        print("servo_1 angle: " + str(servo_1.get_angle()))
 
         servo_1.turn_by_angle(90, 200)
         time.sleep(1) 
@@ -91,5 +91,5 @@ if __name__ == "__main__":
         controller.cleanup()
 
     except Exception as e:
-        print("Class RiApi Error:", str(e))
+        print(traceback.format_exc() + "===> ", str(e))
         sys.exit(2)
